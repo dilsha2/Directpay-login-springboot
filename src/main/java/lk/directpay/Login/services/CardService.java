f3f66398c45d43e741cb9db65e12ad6d973e757e
unknown <diliprasanna96@gmail.com>
2023-07-10 10:03:48 +0530

    update

--- src/main/java/lk/directpay/Login/services/CardService.java
+++ src/main/java/lk/directpay/Login/services/CardService.java
@@ -1,6 +1,10 @@
 package lk.directpay.Login.services;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lk.directpay.Login.entities.Currency;
 import lk.directpay.Login.entities.User;
+import lk.directpay.Login.util.AppConstants;
 import lk.directpay.Login.util.IpgServiceHelper;
 import lk.directpay.Login.util.ResponseDecoder;
 import org.springframework.beans.factory.annotation.Value;
@@ -13,6 +17,7 @@ import javax.crypto.NoSuchPaddingException;
 import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
+import java.util.HashMap;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -58,8 +63,23 @@ public class CardService {
     }
 
     public String getAddingData(User user, String orderId){
-        return "";
+        try {
+            HashMap<String, Object> json = new HashMap<>();
+            json.put("merchant_id", DP_MERCHANT_ID);
+            json.put("amount", "10.00");
+            json.put("type", AppConstants.MERCHANT_CARD_ADD);
+            json.put("order_id", orderId);
+            json.put("currency", Currency.LKR.name());
+            json.put("response_url", "");
+            json.put("return_url", "");
+            json.put("first_name", user.getName());
+            json.put("email", user.getEmail());
+            json.put("page_type", AppConstants.REQUEST_PAGE_TYPE_IN_APP);
+            return ipgServiceHelper.encodeReqBody(new ObjectMapper().writeValueAsString(json));
+        }catch (JsonProcessingException e) {
+            e.printStackTrace();
+            LOGGER.log(Level.SEVERE, e.getMessage());
+            return null;
+        }
     }
-
-
 }
---
+++ src/main/java/lk/directpay/Login/util/AppConstants.java
@@ -0,0 +1,41 @@
+package lk.directpay.Login.util;
+
+public class AppConstants {
+
+
+
+    public static final String CONFIG_NAME_EMAIL = "EMAIL";
+    public static final String CONFIG_NAME_TEL = "TELEPHONE";
+
+    //IPG Service Constants
+    public static final String ITEM_TYPE_PRODUCT = "0";
+    public static final String ITEM_TYPE_SERVICE = "1";
+    public static final String ITEM_TYPE_SUBSCRIPTION = "2";
+    public static final String INVOICE_TYPE = "3";
+
+    public static final String FREE_DELIVARY = "0";
+    public static final String FIXED_DELIVARY_FEE = "1";
+
+    public static final String INTERVAL_MONTHLY = "1";
+    public static final String INTERVAL_BIANNUALLY = "2";
+    public static final String INTERVAL_ANNUALLY = "3";
+    public static final String INTERVAL_QUARTERLY= "4";
+
+    public static final String ITEM_ENABLE = "0";
+    public static final String ITEM_DISABLE = "1";
+    public static final String ITEM_UPDATE = "2";
+    public static final String ITEM_DELETE = "3";
+    public static final String ITEM_CREATE = "4";
+
+    public static final String MERCHANT_CARD_ADD = "CARD_ADD";
+    public static final String MERCHANT_ONE_TIME = "ONE_TIME";
+    public static final String RECURRING = "RECURRING";
+
+    public static final String REQUEST_PAGE_TYPE_IN_APP = "IN_APP";
+  
+    public static final String REGULAR = "/font/Manrope-Regular.ttf";
+    public static final String BOLD = "/font/Manrope-Bold.ttf";
+    public static final String LIGHT = "/font/Manrope-Light.ttf";
+
+    public static final int STARTER_PACKAGE_ID=2;
+}
